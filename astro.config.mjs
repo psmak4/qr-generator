dae163f4a68f6e805906bfd3f18785f2c7f67740
Preston Jones <psmak4@gmail.com>
2026-01-12 02:11:41 -0500

    Automatic sitemap generation with Astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,14 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
+import sitemap from '@astrojs/sitemap';
 import tailwindcss from '@tailwindcss/vite';
 import compress from 'vite-plugin-compression2';
 
 export default defineConfig({
+  site: 'https://thebestqrgenerator.com',
   integrations: [
     react(),
+    sitemap(),
   ],
   vite: {
     plugins: [
